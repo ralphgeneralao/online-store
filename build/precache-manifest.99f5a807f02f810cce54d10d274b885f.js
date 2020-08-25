23c7c0b4a6fecf05d1f6b2413f6241ad1b8e2c9e
Ralph <ralph@dnamicro.com>
2020-08-25 08:12:47 +0800

    broken individual components and styling menu items

---
+++ build/precache-manifest.99f5a807f02f810cce54d10d274b885f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f10c5d8f3c7782d31572230950cd80e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "094700ef98da59ef58bc",
+    "url": "/static/css/main.bfbd454e.chunk.css"
+  },
+  {
+    "revision": "6b05939d26333731a7cb",
+    "url": "/static/js/2.e7982958.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7982958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "094700ef98da59ef58bc",
+    "url": "/static/js/main.15dde7d2.chunk.js"
+  },
+  {
+    "revision": "3d3ede8595dd928f0364",
+    "url": "/static/js/runtime-main.15b1e7b2.js"
+  }
+]);
